1b8a739c5c38d7582b88b45915bc29bf8977d35a
Natalie Cooper <ncooper@tcd.ie>
2018-01-04 12:52:20 +0000

    Edit to remove reps with < 3000 days

--- code/00-fix-simulations-file.R
+++ code/00-fix-simulations-file.R
@@ -1,9 +1,7 @@
 # Fixing up movement model simulations dataset
-# For some reason the data has 1078 replicates not 1000
-# Therefore this code removes the excess 78
-# I've arbitrarily chosen to remove the 78 with the fewest
-# with fewest days in the run < 2957.
-# Natalie Cooper Nov 2017
+# Some of the runs have < 3000 days so these need to be cut
+# before we start (where the simulation got stuck and "stranded")
+# Natalie Cooper Jan 2018
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 # Load libraries
 library(tidyverse)
@@ -12,17 +10,12 @@ library(tidyverse)
 # Takes a bit of time
 resTrack <- read_csv("data/model.sims.csv")
 
-# Remove rep that didn't work
-resTrack <- resTrack[-c(3213051:3213211), ]
-
-# Remove excess 78 simulations
-# Remove those with fewest days - < 2957
-
+# Remove reps with < 3000 days
 remove <- 
   resTrack %>%
   group_by(Rep) %>%
   summarise(ln = length(count2)) %>%
-  filter(ln < 2957)
+  filter(ln < 3000)
 
 toremove <- match(resTrack$Rep, remove$Rep, nomatch = 0)
 
@@ -30,4 +23,4 @@ resTrack2 <- resTrack[toremove == 0, ]
 
 # Write to data folder
 # Use this for later analyses
-write.csv(resTrack2, "data/model.sims1000.csv", quote = FALSE, row.names = FALSE)
+write.csv(resTrack2, "data/model.sims.3000.csv", quote = FALSE, row.names = FALSE)
