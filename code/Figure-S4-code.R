981677a241f334dbc5c4eeccfe507a04e5ea6042
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2018-01-24 16:06:42 +0000

    Edits to FigS4 code with new data added

--- code/Figure-S4-code.R
+++ code/Figure-S4-code.R
@@ -1,7 +1,7 @@
 # Author: Clive Trueman
 # Adapted by: Andrew Jackson 19-Jun-2017
-# About: script to generate figures S4a-d for the blue whale paper
-# Tidied by Natalie Cooper Nov 2017
+# About: script to generate figures S4 for the blue whale paper
+# Tidied by Natalie Cooper Jan 2017
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 library(tidyverse)
@@ -13,16 +13,54 @@ library(lubridate)
 cnst <- 6
 
 # Read in resident whale model data
-resNor <- read.csv("data/Norway.csv", header = TRUE)
-resIre <- read.csv("data/west.Ireland.csv", header = TRUE)
-resCan <- read.csv("data/Canaries.csv", header = TRUE)
-resMidAtl <- read.csv("data/Mid_Atl.csv", header = TRUE)
+resNor <- read.csv("data/Norway.Res.TL.csv", header = TRUE)
+resIre <- read.csv("data/Ireland.Res.TL.csv", header = TRUE)
+resCan <- read.csv("data/Canaries.Res.TL.csv", header = TRUE)
+resMidAtl <- read.csv("data/Atl.Res.TL.csv", header = TRUE)
+resCV <- read.csv("data/CV.TL.csv", header = TRUE)
+#resMaur <- read.csv("data/Maur.TL.csv", header = TRUE)
+
+# Remove B.state variable as it messes up bind.rows
+resNor <- 
+  resNor %>%
+  select(-B.state)
+
+resNor <- 
+  resNor %>%
+  select(-B.state)
+
+resIre <- 
+  resIre %>%
+  select(-B.state)
+
+resCan <- 
+  resCan %>%
+  select(-B.state)
+
+resMidAtl <- 
+  resMidAtl %>%
+  select(-B.state)
+
+#resMaur <- 
+#  resMaur %>%
+#  select(-B.state)
+
+# Here count is correct, but Day.No is not
+# so switch them to get the next steps to work
+# Also shift up start to 120 days to match others
+resCV <- 
+  resCV %>%
+  select(-B.state, -Day.No) %>%
+  mutate(Day.No = count + 119)
+  
 
 # Combine datasets
 res_all <- bind_rows(list(Norway = resNor, 
                           Ireland = resIre, 
                           Canaries = resCan, 
-                          "Mid-Atlantic" = resMidAtl), .id = "Region")
+                          "Mid-Atlantic" = resMidAtl,
+                          #"Mauritania" = resMaur,
+                          "Cape Verde" = resCV), .id = "Region")
 
 # Remove lines with no d13C values
 res_all <- res_all[which(!is.na(res_all$d13C)), ]
@@ -39,8 +77,10 @@ tmp2 <- res_all %>% group_by(Region, Rep)  %>%
                    Day.No = .$Day.No))
 
 # re-order the Region Factor
-tmp2$Region <- factor(tmp2$Region, levels = c("Ireland","Norway",
-                                              "Canaries","Mid-Atlantic"))
+tmp2$Region <- factor(tmp2$Region, levels = c("west Ireland","Norwegian Sea",
+                                              "Canaries","Mid-Atlantic",
+                                              #"Mauritania",
+                                              "Cape Verde"))
 
 # Plot and facet wrap by region
 sim_facet <- 
@@ -53,6 +93,6 @@ sim_facet <-
           strip.text = element_text(size = NULL)) +
     facet_wrap(~Region, ncol = 2)
 
-ggsave("manuscript/revision/figures/Figure-S4-facet-wrap-d13C.png", sim_facet, 
+ggsave("manuscript/revision/figures/Figure-S4-regions-d13C.png", sim_facet, 
        device = png(width = 600, height = 400))
 
